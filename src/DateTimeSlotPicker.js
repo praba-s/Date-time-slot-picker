e679cba5cff0f9a463912fa7388734ba73f4d982
karunakar-s <Aug@2017>
2019-07-12 12:36:48 +0530

    API calls added

--- src/DateTimeSlotPicker.js
+++ src/DateTimeSlotPicker.js
@@ -3,7 +3,14 @@ import { DateTimePicker } from "./components/DateTimePicker";
 import slots from "./slots";
 import moment from "moment";
 
-export class DateTimeSlotPicker extends Component{
+/*
+import {buildUrl} from "../../actions";
+import {TicketPolicyActions} from "../../constants";
+import axios from "axios";
+import config from "../../config";*/
+
+
+export default class DateTimeSlotPicker extends Component{
     constructor (props) {
         super(props)
         this.state ={
@@ -14,7 +21,7 @@ export class DateTimeSlotPicker extends Component{
     }
 
     componentDidMount() {
-        this.getAvailableTimeSlots();
+        //this.getAvailableTimeSlots(this.state.selectedDateValue)
     }
 
     onDateChange = (date) => {
@@ -37,6 +44,38 @@ export class DateTimeSlotPicker extends Component{
 
     }
 
+    /*onDateChange = (date) => {
+        console.log("On Date Change  " + date)
+        if(date === ''){
+            return
+        }
+        this.setState((state, props) => {
+            return {
+                availableTimeSlots : [],
+                showTimeSlotPanel : false,
+                selectedDateValue: date
+            }
+        })
+        this.getAvailableTimeSlots(date)
+    }
+
+    getAvailableTimeSlots(date){
+        let dateString  = moment(date).format("YYYY-MM-DD")
+        console.log("selected Date: " + dateString);
+        axios.get(buildUrl(config.endpoints.getOpenSlots(dateString))).then(({ data: openSlots }) => {
+            this.setState((state, props) => {
+                  return {
+                      availableTimeSlots: this.parseTimeSlot(openSlots.start_times),
+                      showTimeSlotPanel: true,
+                  }
+              }
+            )}).catch((error) => {
+            const { status: statusCode } = error.response
+            const err = status.error(error, 'There was an error fetching your notification. Please try again later.')
+
+        })
+    }*/
+
     getAvailableTimeSlots(){
         let slotsFromServer = slots;
         let availableTimeSlots = [];
@@ -51,17 +90,30 @@ export class DateTimeSlotPicker extends Component{
         return availableTimeSlots;
     }
 
-    render () {
+    parseTimeSlot(slots){
+        let slotsFromServer = slots;
+        let availableTimeSlots = [];
+        let amSlot = [];
+        let pmSlot = [];
+        slotsFromServer.map(time => {
+            let momentDate = moment.utc(time, "YYYY-MM-DDThh:mm:ssTZD");
+            let timeStr = momentDate.format("hh:mm a")
+            timeStr.indexOf("am") > 1 ? amSlot.push(momentDate.format("hh:mm")) : pmSlot.push(momentDate.format("hh:mm"))
+        })
+        availableTimeSlots.push(amSlot); availableTimeSlots.push(pmSlot);
+        return availableTimeSlots;
+    }
+
+    render() {
         return (
-            <div>
-                <DateTimePicker
-                          onChange={this.onDateChange}
-                          showTimeSlotPanel = {this.state.showTimeSlotPanel}
-                          availableTimeSlots = { this.state.availableTimeSlots }
-                          format="MM/DD/YYYY hh:mm a"
-                >
-                </DateTimePicker>
-            </div>
+          <div>
+              <DateTimePicker format="MM/DD/YYYY hh:mm a"
+                              onChange={this.onDateChange}
+                              showTimeSlotPanel = {this.state.showTimeSlotPanel}
+                              availableTimeSlots = { this.state.availableTimeSlots }
+              >
+              </DateTimePicker>
+          </div>
         )
     }
 }
